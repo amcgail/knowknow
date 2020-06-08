7182128be97bc0926aa834e11c93186b96fe809e
Alec McGail <amcgail2@gmail.com>
2020-06-08 18:24:16 -0400

    reorganize

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class InstallWrapper(install):
 
 setuptools.setup(
     name="knowknow-amcgail", # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Alec McGail",
     author_email="amcgail2@gmail.com",
     description="Analyzing the evolution of ideas using citation analysis",
